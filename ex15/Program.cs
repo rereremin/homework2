a8d186c1812628f78b4402d1bcf69f76fb1d325f
nikita <zherko.na@phystech.edu>
2022-11-20 02:33:20 +0300

    Add ex 15

---
+++ ex15/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает цифру соответствующую дню недели и определяет выходной ли он?
+System.Console.WriteLine("Выходной ли день?");
+System.Console.Write("Какой день по счету? ");
+string UserInput = Console.ReadLine();
+
+int day = int.Parse(UserInput);
+
+if(day == 6 | day == 7) 
+{
+    System.Console.WriteLine($"{day} -> выходной");
+}
+else if(day > 0 && day < 6) 
+{
+    System.Console.WriteLine($"{day} -> рабочий");
+}
+else
+{
+    System.Console.WriteLine($"{day} -> не день недели");
+}
