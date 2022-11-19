fed2cbedcf00db5d9eb2eeed7f549b8cf3da12e2
nikita <zherko.na@phystech.edu>
2022-11-20 02:17:27 +0300

    Add ex 13

---
+++ ex13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вывести третью цифру числа, если она имеется
+System.Console.WriteLine("Третья цифра в числе?");
+System.Console.Write("Введите число: ");
+string UserInput = Console.ReadLine();
+
+int length = UserInput.Length;
+int number = int.Parse(UserInput);
+
+int CurrentNumber;
+double a = Math.Pow(10, length - 3);
+if(length >= 3)
+{
+    CurrentNumber = number / (int)a % 10;
+    System.Console.WriteLine($"{number} -> {CurrentNumber}");
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет.");
+}
+
