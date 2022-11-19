84777b63497c40585957b897f90db6be6a774473
nikita <zherko.na@phystech.edu>
2022-11-20 01:28:40 +0300

    Add Ex 10

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход трехзначное число и выдает его вторую цифру
+
+System.Console.WriteLine("Прграмма выдает вторую цифру числа.");
+
+System.Console.Write("Введите трехзначное число: ");
+string UserInput = Console.ReadLine();
+int number = int.Parse(UserInput);
+
+int CurrentNumber = number / 10 % 10;
+System.Console.WriteLine($"{number} -> {CurrentNumber}");
